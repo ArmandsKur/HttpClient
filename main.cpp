060718f223421acd41c9e56f6fe581cf0a185dd5
Armands Kurevskis <armandskur@gmail.com>
2025-11-13 22:54:32 +0200

    added functionality to find Ip addresses for hostname

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
 
 int main(const int argc, char* argv[] ) {
     //Must be only one argument provided
@@ -89,5 +93,78 @@ int main(const int argc, char* argv[] ) {
     } while (true);
 
     std::cout << "Host: " << argv[1] << std::endl;
+
+
+    struct addrinfo hints; //Create a addrinfo structure for hints
+    struct addrinfo *result, *rp; //Create addrinfo pointers for furure results and result poninter
+
+    //Initialize all hints as 0
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;    // Allow IPv4 or IPv6
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_ADDRCONFIG;
+    hints.ai_protocol = 0;          // Any protocol
+
+    int rc; //Get result code of the getaddrinfo
+    rc = getaddrinfo(argv[1], "80", &hints, &result);
+    if (rc != 0) {
+        std::cerr << gai_strerror(rc) << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    int v6_addr_count = 0;
+    int v4_addr_count = 0;
+    int total_addr_count = 0;
+
+    //Create pointers which will be used to point to objects of types sockaddr_in and sockaddr_in6
+    struct sockaddr_in *sockaddr_ipv4;
+    struct sockaddr_in6 *sockaddr_ipv6;
+    size_t ipbufferlength;
+    char *ipbuffer = nullptr;
+
+    std::cout << "Resolved addresses for " << hostname << ":"<<std::endl;
+
+    //Iterate trough linked list of all found addresses
+    for (rp = result; rp != NULL; rp = rp->ai_next) {
+        //If ai_family is AF_INET then this ipv4 address
+        if (rp->ai_family == AF_INET) {
+            v4_addr_count++;
+            sockaddr_ipv4 = (struct sockaddr_in *)rp->ai_addr;
+            ipbufferlength = INET_ADDRSTRLEN; //ipv4 buffer should hold 16 chars
+
+            ipbuffer = new char[ipbufferlength];
+
+            auto results = inet_ntop(AF_INET, &sockaddr_ipv4->sin_addr, ipbuffer, ipbufferlength);
+
+            if (results == nullptr) {
+                std::cout << "IPv4: <invalid>" << std::endl;
+            } else {
+                std::cout <<"IPv4: "<< ipbuffer << std::endl;
+            }
+
+        } else if (rp->ai_family == AF_INET6) { // AF_INET6 is for ipv6
+            v6_addr_count++;
+            sockaddr_ipv6 = (struct sockaddr_in6 *)rp->ai_addr;
+            ipbufferlength = INET6_ADDRSTRLEN; //ipv6 buffer should hold 46 chars
+
+            ipbuffer = new char[ipbufferlength];
+
+            auto results = inet_ntop(AF_INET6, &sockaddr_ipv6->sin6_addr,ipbuffer, ipbufferlength);
+            if (results == nullptr) {
+                std::cout << "IPv6: <invalid>" << std::endl;
+            } else {
+                std::cout <<"IPv6: "<< ipbuffer << std::endl;
+            }
+        }
+        total_addr_count++;
+        delete[] ipbuffer;
+    }
+    if (v4_addr_count + v6_addr_count == 0) {
+        std::cout << "no printable addresses for AF_INET/AF_INET6" << std::endl;
+    }
+    std::cout<< "Resolved " << total_addr_count << " addresses (" << v4_addr_count << " IPv4, " << v6_addr_count << " IPv6)" << std::endl;
+
+    freeaddrinfo(result);
+
     return 0;
 }
